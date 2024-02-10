43fe26debb2c99747120adee61f81b10c27c2315
Aldric Ducreux <neogeekmo@live.fr>
2024-02-10 17:42:11 +0100

    Init File

--- Core.lua
+++ Core.lua
@@ -24,23 +24,30 @@ function AprRC:OnInitialize()
     C_ChatInfo.RegisterAddonMessagePrefix("AprRCChat")
 end
 
--- - Faction
--- - Race
--- - Gender
--- - Class
--- - HasAchievement
--- - DontHaveAchievement
--- - ExitTutorial
--- - PickUp
--- - PickUpDB
--- - DropQuest
--- - Qpart
+-- - Faction ["Faction"] = "Horde" (UnitFactionGroup("player"))
+-- - Race    ["Race"] = "Gnome"
+-- - Gender  ["Gender"] = 2
+-- - Class   ["Class"] = "DRUID"
+-- - Grind   command grind lvl
+-- - HasAchievement command HasAchievement ID
+-- - DontHaveAchievement command DontHaveAchievement ID
+
+-- - ExitTutorial ["ExitTutorial"] = 62567 (IsOnQuest(questID)
+-- - PickUp       ["PickUp"] = { questID1, questID2}
+-- - PickUpDB     ["PickUpDB"] = { questID1, questID2}
+-- - DropQuest    ["DropQuest"] = 62567 (questID)
+-- - Qpart        ["Qpart"] = {[46727] = {["2"] = "1",},
 -- - QpartDB
 -- - QpartPart
--- - Treasure
--- - QaskPopup
--- - Done
--- - DoneDB
+-- - Fillers
+-- - Done       ["Done"] = { questID1, questID2}
+-- - DoneDB     ["DoneDB"] = { questID1, questID2}
+-- - Treasure   ["Treasure"] = 31401 (questID)
+
+-- - Group      ["Group"] = { Number = 3, QuestId = 51384},
+-- - GroupTask  ["GroupTask"] = 51384, (the questId from Group, step to check if player want to do the group quest)
+-- - QuestLineSkip ???? (block group quest if present) ["QuestLineSkip"] = 51226,
+
 -- - Waypoint
 -- - SetHS
 -- - UseHS
@@ -52,14 +59,11 @@ end
 -- - NodeID
 -- - Name
 -- - WarMode
--- - Grind
 -- - ZoneDoneSave
 -- - ZoneStepTrigger
 -- - Range
 -- - Coord
--- - QuestLineSkip
--- - Group
--- - GroupTask
+
 -- - ExtraLineText
 -- - ExtraLine
 -- - LoaPick
@@ -77,7 +81,6 @@ end
 -- - InVehicle
 -- - DoIHaveFlight
 -- - DroppableQuest
--- - Fillers
 -- - Dontskipvid
 -- - RaidIcon
 -- - Button
@@ -85,92 +88,11 @@ end
 -- - Gossip
 -- - ChromiePick
 -- - SpellTrigger
+-- - NoAutoFlightMap
+-- - DenyNPC
 
 -- -- only in route check if needed
 -- - ExtraActionB
 
 -- - ButtonSpellId -- Deleted
 -- - BlockQuests -- Deleted
-
-AprRC.EventFrame = CreateFrame("Frame")
-AprRC.EventFrame:RegisterAllEvents("ADDON_LOADED")
-AprRC.EventFrame:RegisterEvent("BAG_UPDATE_DELAYED")
-AprRC.EventFrame:RegisterEvent("BANKFRAME_OPENED")
-AprRC.EventFrame:RegisterEvent("CHAT_MSG_ADDON")
-AprRC.EventFrame:RegisterEvent("CHAT_MSG_COMBAT_XP_GAIN")
-AprRC.EventFrame:RegisterEvent("CHAT_MSG_MONSTER_SAY")
-AprRC.EventFrame:RegisterEvent("CHAT_MSG_SYSTEM")
-AprRC.EventFrame:RegisterEvent("COMPANION_LEARNED")
-AprRC.EventFrame:RegisterEvent("COMPANION_UNLEARNED")
-AprRC.EventFrame:RegisterEvent("COMPANION_UPDATE")
-AprRC.EventFrame:RegisterEvent("CONFIRM_BINDER")
-AprRC.EventFrame:RegisterEvent("CONFIRM_XP_LOSS")
-AprRC.EventFrame:RegisterEvent("CVAR_UPDATE")
-AprRC.EventFrame:RegisterEvent("GOSSIP_CLOSED")
-AprRC.EventFrame:RegisterEvent("GOSSIP_CONFIRM_CANCEL")
-AprRC.EventFrame:RegisterEvent("GOSSIP_SHOW")
-AprRC.EventFrame:RegisterEvent("GROUP_JOINED")
-AprRC.EventFrame:RegisterEvent("GROUP_LEFT")
-AprRC.EventFrame:RegisterEvent("HEARTHSTONE_BOUND")
-AprRC.EventFrame:RegisterEvent("ITEM_PUSH")
-AprRC.EventFrame:RegisterEvent("LEARNED_SPELL_IN_TAB")
-AprRC.EventFrame:RegisterEvent("MERCHANT_CLOSED")
-AprRC.EventFrame:RegisterEvent("MERCHANT_SHOW")
-AprRC.EventFrame:RegisterEvent("MINIMAP_UPDATE_ZOOM")
-AprRC.EventFrame:RegisterEvent("NEW_PET_ADDED")
-AprRC.EventFrame:RegisterEvent("NEW_WMO_CHUNK")
-AprRC.EventFrame:RegisterEvent("PET_BATTLE_CLOSE")
-AprRC.EventFrame:RegisterEvent("PET_BATTLE_OPENING_START")
-AprRC.EventFrame:RegisterEvent("PET_STABLE_CLOSED")
-AprRC.EventFrame:RegisterEvent("PET_STABLE_SHOW")
-AprRC.EventFrame:RegisterEvent("PLAYER_CHOICE_UPDATE")
-AprRC.EventFrame:RegisterEvent("PLAYER_CONTROL_LOST")
-AprRC.EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-AprRC.EventFrame:RegisterEvent("PLAYER_LEVEL_UP")
-AprRC.EventFrame:RegisterEvent("PLAYER_MONEY")
-AprRC.EventFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
-AprRC.EventFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
-AprRC.EventFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
-AprRC.EventFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
-AprRC.EventFrame:RegisterEvent("PLAYER_XP_UPDATE")
-AprRC.EventFrame:RegisterEvent("QUEST_ACCEPTED")
-AprRC.EventFrame:RegisterEvent("QUEST_ACCEPT_CONFIRM")
-AprRC.EventFrame:RegisterEvent("QUEST_AUTOCOMPLETE")
-AprRC.EventFrame:RegisterEvent("QUEST_COMPLETE")
-AprRC.EventFrame:RegisterEvent("QUEST_DETAIL")
-AprRC.EventFrame:RegisterEvent("QUEST_FINISHED")
-AprRC.EventFrame:RegisterEvent("QUEST_GREETING")
-AprRC.EventFrame:RegisterEvent("QUEST_LOG_UPDATE")
-AprRC.EventFrame:RegisterEvent("QUEST_PROGRESS")
-AprRC.EventFrame:RegisterEvent("QUEST_REMOVED")
-AprRC.EventFrame:RegisterEvent("QUEST_TURNED_IN")
-AprRC.EventFrame:RegisterEvent("REQUEST_CEMETERY_LIST_RESPONSE")
-AprRC.EventFrame:RegisterEvent("SKILL_LINES_CHANGED")
-AprRC.EventFrame:RegisterEvent("SPELL_UPDATE_COOLDOWN")
-AprRC.EventFrame:RegisterEvent("TAXIMAP_OPENED")
-AprRC.EventFrame:RegisterEvent("TOYS_UPDATED")
-AprRC.EventFrame:RegisterEvent("TRAINER_CLOSED")
-AprRC.EventFrame:RegisterEvent("TRAINER_SHOW")
-AprRC.EventFrame:RegisterEvent("TRAINER_UPDATE")
-AprRC.EventFrame:RegisterEvent("UI_ERROR_MESSAGE")
-AprRC.EventFrame:RegisterEvent("UI_INFO_MESSAGE")
-AprRC.EventFrame:RegisterEvent("UNIT_AURA")
-AprRC.EventFrame:RegisterEvent("UNIT_ENTERED_VEHICLE")
-AprRC.EventFrame:RegisterEvent("UNIT_ENTERING_VEHICLE")
-AprRC.EventFrame:RegisterEvent("UNIT_EXITING_VEHICLE")
-AprRC.EventFrame:RegisterEvent("UNIT_QUEST_LOG_CHANGED")
-AprRC.EventFrame:RegisterEvent("UNIT_SPELLCAST_START")
-AprRC.EventFrame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
-AprRC.EventFrame:RegisterEvent("UNIT_TARGET")
-AprRC.EventFrame:RegisterEvent("UPDATE_FACTION")
-AprRC.EventFrame:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
-AprRC.EventFrame:RegisterEvent("UPDATE_UI_WIDGET")
-AprRC.EventFrame:RegisterEvent("VEHICLE_UPDATE")
-AprRC.EventFrame:RegisterEvent("ZONE_CHANGED")
-AprRC.EventFrame:RegisterEvent("ZONE_CHANGED_INDOORS")
-AprRC.EventFrame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-AprRC.EventFrame:SetScript("OnEvent", function(self, event, ...)
-    if event then
-        return
-    end
-end)
