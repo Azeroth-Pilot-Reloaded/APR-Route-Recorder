c7346918b12e4d0a27090096c6c2199960fd92f4
Aldric Ducreux <neogeekmo@live.fr>
2024-08-04 23:32:02 +0200

    remove range for the qpartpart

--- Commands.lua
+++ Commands.lua
@@ -229,7 +229,7 @@ function AprRC.command:SlashCmd(input)
                             TrigText = text,
                             QpartPart = { [questID] = { objectiveID } }
                         }
-                        AprRC:SetStepCoord(step, 5)
+                        AprRC:SetStepCoord(step)
                         AprRC:NewStep(step)
                         print("|cff00bfffQpartPart - [" ..
                             C_QuestLog.GetTitleForQuestID(questID) .. "] - " .. objectiveID .. "|r Added")
