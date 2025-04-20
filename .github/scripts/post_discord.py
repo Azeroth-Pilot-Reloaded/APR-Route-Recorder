81bbb5481544882e90f11665423e172825c223dd
Aldric Ducreux <neogeekmo@live.fr>
2025-04-20 23:46:42 +0200

    Update Discord post with APRRC Data

--- .github/scripts/post_discord.py
+++ .github/scripts/post_discord.py
@@ -33,7 +33,7 @@ if lines:
     lines = lines[1:]
 
 # Create a new header line
-header_line = f"## Patch Note - Version {tag_name} <:apr:1271743726225719296>"
+header_line = f"## Patch Note - AprRC - Version {tag_name} <:aprrc:1267547229741387868>"
 
 # Rebuild the body with our custom first line
 new_release_body = header_line + "\n" + "\n".join(lines)
@@ -44,6 +44,11 @@ new_release_body = re.sub(
     r'**Full Changelog**: [\1](https://github.com/Azeroth-Pilot-Reloaded/azeroth-pilot-reloaded/compare/\1)',
     new_release_body
 )
+
+# Remove unnecessary newline(s) before header lines (lines starting with '#')
+# This regex replaces any occurrence of a newline, optional whitespace, and another newline
+# when it is immediately followed by a '#' with a single newline.
+new_release_body = re.sub(r'\n\s*\n(?=\#)', '\n', new_release_body)
 
 # Build the message to post on Discord
 message = new_release_body
